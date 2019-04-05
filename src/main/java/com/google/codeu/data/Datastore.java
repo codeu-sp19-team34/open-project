58de94a535792c81acd5c4e51a55f70789faf7fb
Sebrianne Ferguson <24661557+sebriannef@users.noreply.github.com>
2019-04-04 23:11:11 -0700

    Initial commit
    changed the codeu-starter-project to open-project in the files
    took out the servlet, js, and html files for stats page (don't think we'll be needing that)

---
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.google.codeu.data;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/** Provides access to the data stored in Datastore. */
+public class Datastore {
+
+  private DatastoreService datastore;
+
+  public Datastore() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
+
+  /** Stores the Message in Datastore. */
+  public void storeMessage(Message message) {
+    Entity messageEntity = new Entity("Message", message.getId().toString());
+    messageEntity.setProperty("user", message.getUser());
+    messageEntity.setProperty("text", message.getText());
+    messageEntity.setProperty("timestamp", message.getTimestamp());
+    messageEntity.setProperty("recipient", message.getRecipient());
+    messageEntity.setProperty("score", message.getScore());
+
+    datastore.put(messageEntity);
+  }
+
+  /**
+   * Gets messages posted on the page of a specific recipient.
+   *
+   * @return a list of messages posted on the page of the page the user is viewing, or empty list if if there
+   *         are not any messages for the recipient. List is sorted by time descending.
+   */
+  public List<Message> getMessages(String recipient) {
+
+    List<Message> messages = new ArrayList<>();
+
+    Query query =
+        new Query("Message")
+            .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
+            .addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+
+        float sentimentScore = entity.getProperty("score") == null? (float) 0.0 : ((Double) entity.getProperty("score")).floatValue();
+
+        Message message = new Message(id, user, text, timestamp, recipient, sentimentScore);
+
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
+
+    return messages;
+  }
+
+  /** Returns the total number of messages for all users. */
+  public int getTotalMessageCount(){
+    Query query = new Query("Message");
+    PreparedQuery results = datastore.prepare(query);
+    return results.countEntities(FetchOptions.Builder.withLimit(1000));
+  }
+
+  /** Returns all message **/
+  public List<Message> getAllMessages() {
+    List<Message> messages = new ArrayList<>();
+
+    Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    for(Entity entity: results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+        String recipient = (String) entity.getProperty("recipient");
+        float sentimentScore = entity.getProperty("score") == null? (float) 0.0 : ((Double) entity.getProperty("score")).floatValue();
+
+        Message message = new Message(id, user, text, timestamp, recipient, sentimentScore);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
+
+    return messages;
+  }
+
+  /** Stores the User in Datastore. */
+  public void storeUser(User user) {
+    Entity userEntity = new Entity("User", user.getEmail());
+    userEntity.setProperty("email", user.getEmail());
+    userEntity.setProperty("aboutMe", user.getAboutMe());
+    datastore.put(userEntity);
+  }
+
+  /**
+   * Returns the User owned by the email address, or
+   * null if no matching User was found.
+   */
+  public User getUser(String email) {
+
+    Query query = new Query("User")
+            .setFilter(new Query.FilterPredicate("email", FilterOperator.EQUAL, email));
+    PreparedQuery results = datastore.prepare(query);
+    Entity userEntity = results.asSingleEntity();
+    if(userEntity == null) {
+      return null;
+    }
+
+    String aboutMe = (String) userEntity.getProperty("aboutMe");
+    User user = new User(email, aboutMe);
+
+    return user;
+  }
+
+}
---
+++ src/main/java/com/google/codeu/data/Message.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.google.codeu.data;
+
+import java.util.UUID;
+
+/** A single message posted by a user. */
+public class Message {
+
+  private UUID id;
+  private String user;
+  private String text;
+  private long timestamp;
+  private String recipient;
+  private float score;
+
+  /**
+   * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
+   * random ID and uses the current system time for the creation time.
+   */
+  public Message(String user, String text, String recipient, float score) {
+    this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient, score);
+  }
+
+  public Message(UUID id, String user, String text, long timestamp, String recipient, float score) {
+    this.id = id;
+    this.user = user;
+    this.text = text;
+    this.timestamp = timestamp;
+    this.recipient = recipient;
+    this.score = score;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public String getRecipient() {
+    return recipient;
+  }
+
+  public void setText(String translatedText) {
+    this.text = translatedText;
+  }
+
+  public float getScore() {
+        return score;
+    }
+}
