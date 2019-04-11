8407e70a988d0fc6ecbc38d2d85d017e7dd278a3
Horacio Lopez <hlopez3rd@gmail.com>
2019-04-10 22:41:59 -0700

    Began messages servlet to get messages to begin appearing in plain text

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,40 +1,67 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-import java.util.List;
 
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Message;
-import com.google.gson.Gson;
+@SuppressWarnings("serial")
+@WebServlet(name = "CloudSQL",
+    description = "CloudSQL: Write messages from open_project_db to CloudSQL",
+    urlPatterns = "/feed")
 
-/*
- * Handles fetching all messages for the public feed
- */
-@WebServlet("/feed")
-public class MessageFeedServlet extends  HttpServlet{
-    private Datastore datastore;
+public class MessagesFeedServlet extends HttpServlet {
+  Connection connect;
 
-    @Override
-    public void init() {
-        datastore = new Datastore();
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
+      ServletException {
+
+    final String selectSql = "SELECT * FROM open_project_db.messages";
+
+    PrintWriter out = resp.getWriter();
+    resp.setContentType("text/plain");
+
+    try (ResultSet rs = connect.prepareStatement(selectSql).executeQuery()) {
+      out.print("Messages:\n");
+      while (rs.next()) {
+        String id = rs.getString("id");
+        String firstName = rs.getString("user_id");
+        String lastName = rs.getString("message");
+
+        out.println(id + ": " + user_id + " " + message);
+      }
+    } catch (SQLException e) {
+      throw new ServletException("SQL error", e);
     }
 
-    /**
-     * Responds with JSON representation of Message data for all users
-     */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
+  }
 
-        List<Message> messages = datastore.getAllMessages();
-        Gson gson = new Gson();
-        String json = gson.toJson(messages);
-
-        response.getOutputStream().println(json);
+  @Override
+  public void init() throws ServletException {
+    String url = System.getProperty("cloudsql");
+    log("connecting to: " + url);
+    try {
+      connect = DriverManager.getConnection(url);
+    } catch (SQLException e) {
+      throw new ServletException("Unable to connect to Cloud SQL", e);
     }
+  }
 }
