429eed5c3ff5d0730c6bce56f73837d18be72d97
Joel Ramos <jrcs.portfolio@gmail.com>
2019-04-13 14:06:19 -0700

    pages for study group finder

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -38,7 +38,7 @@ function addLoginOrLogoutLinkToNavigation() {
               createListItem(createLink('/logout', 'Logout')));
         } else {
           navigationElement.appendChild(
-              createListItem(createLink('/login', 'Login')));
+              createListItem(createLink('/login.html', 'Login')));
         }
       });
 }
@@ -50,6 +50,7 @@ function addLoginOrLogoutLinkToNavigation() {
  */
 function createListItem(childElement) {
   const listItemElement = document.createElement('li');
+  listItemElement.className += "nav-item";
   listItemElement.appendChild(childElement);
   return listItemElement;
 }
@@ -62,6 +63,7 @@ function createListItem(childElement) {
  */
 function createLink(url, text) {
   const linkElement = document.createElement('a');
+  linkElement.className += "nav-link";
   linkElement.appendChild(document.createTextNode(text));
   linkElement.href = url;
   return linkElement;
